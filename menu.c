7bbacf3e68e9ff5e5430fca275bc5429ddc20f46
emessonSilva <140443316+emessonSilva@users.noreply.github.com>
2023-07-24 16:43:25 -0300

    Add files via upload

---
+++ menu.c
@@ -0,0 +1,14 @@
+// Bibliotecas usadas para rodar o programa corretamente
+#include "cores.h"
+#include <stdio.h>
+
+// Criação da função menu
+void menu() {
+  printf("\n Lista de Contatos    \n\n");
+  printf("1. Cadastrar contato\n");
+  printf("2. Listar contatos\n");
+  printf("3. Atualizar contato\n");
+  printf("4. Excluir contato\n");
+  printf("5. Sair\n");
+  printf("Escolha uma opção: ");
+}
